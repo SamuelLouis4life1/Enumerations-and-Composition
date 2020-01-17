9053ee6a08194f06a9b3c2d9f9ed77c55c4d029c
Samuel Louis <sampeur4life1@gmail.com>
2020-01-17 15:23:14 -0300

    Second commit about Enumerations and Composition in C Sharp

---
+++ PostAndComment/Entities/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostAndComment.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Like { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int like)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Like = like;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Title);
+            stringBuilder.Append(Like);
+            stringBuilder.Append(" Likes - ");
+            stringBuilder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine("Comments:");
+            foreach (Comment comment in Comments)
+            {
+                stringBuilder.AppendLine(comment.Text);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ PostAndComment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using PostAndComment.Entities;
+
+namespace PostAndComment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip !!!");
+            Comment c2 = new Comment("Wow that's awesome !!!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+
+            Comment c3 = new Comment("Good night to all !!!");
+            Comment c4 = new Comment("May the force be with you !!!");
+            Post p2 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Good night guys",
+                "See you all tomorrow !!",
+                124);
+            p1.AddComment(c3);
+            p1.AddComment(c4);
+
+            Console.Write(p1);
+            Console.Write(p2);
+
+            Console.ReadLine();
+        }
+    }
+}
