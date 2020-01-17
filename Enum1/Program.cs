14e95e63b80d7fe34188a8253b80e7769cbcbf94
Samuel Louis <sampeur4life1@gmail.com>
2020-01-17 14:42:47 -0300

    Inicial commit about Enumerations and Composition in C Sharp

---
+++ Enum1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Enum1.Entities.Enums;
+using Enum1.Entities;
+
+namespace Enum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            // convert enum to string 
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            // convert string to enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+            Console.ReadLine();
+        }
+    }
+}
